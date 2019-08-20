a832eadd7df99f3644ef21968ea615ec6e4828c0
Ricardo Hildebrand <ricardo.hildebrand@turbinekreuzberg.com>
2019-08-20 10:52:26 +0200

    change data

--- tests/Zed/Fixtures/ZedData.js
+++ tests/Zed/Fixtures/ZedData.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: `http://${process.env.ZED_GLOBAL_DOMAIN || 'zed.global-fashion.local'}`,
-  user: 'admin@spryker.com',
-  password: 'change123',
+  url: `http://${process.env.ZED_DOMAIN}`,
+  user: 'admin',
+  password: 'pass123',
 };
